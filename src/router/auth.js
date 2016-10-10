17c3261a7fadbdb610a03f4bafcfe376c30bc6a0
Meet Godhani <meet_godhani1@yahoo.com>
2016-10-10 23:45:27 +0530

    Fixes

--- src/router/auth.js
+++ src/router/auth.js
@@ -5,6 +5,7 @@ export default {
   },
   logout (cb){
     localStorage.removeItem('token');
+    localStorage.removeItem('user');
   },
   loggedIn () {
     return !!localStorage.token
--- sw-handler.js
+++ sw-handler.js
@@ -13,6 +13,7 @@ self.addEventListener('activate', function(e) {
   )
   return self.clients.claim();
 })
+
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
   var dataUrl = 'https://readr.meetgodhani.com/api/';
@@ -27,10 +28,10 @@ self.addEventListener('fetch', function(e) {
     );
   } else {
     /*
-     * The app is asking for app shell files. In this scenario the app uses the
-     * "Cache, falling back to the network" offline strategy:
-     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
-     */
+    * The app is asking for app shell files. In this scenario the app uses the
+    * "Cache, falling back to the network" offline strategy:
+    * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+    */
     e.respondWith(
       caches.match(e.request).then(function(response) {
         return response || fetch(e.request);
